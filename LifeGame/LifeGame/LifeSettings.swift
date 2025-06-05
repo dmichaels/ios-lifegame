9236579b4669a712da40198d199ccc7f80ed4bae
David Michaels <dmichaels@gmail.com>
2025-06-05 11:38:33 -0400

    etc

--- LifeGame/LifeGame/LifeSettings.swift
+++ LifeGame/LifeGame/LifeSettings.swift
@@ -3,12 +3,12 @@ import CellGridView
 
 public class DefaultLifeSettings
 {
-    public static let cellActiveColor: CellColor = CellColor(Color.red)
-    public static let cellInactiveColor: CellColor = CellColor(Color.white)
+    public static let cellActiveColor: CellColor = CellColor.red
+    public static let cellInactiveColor: CellColor = CellColor.white
 }
 
 class LifeSettings: Settings
 {
-    @Published var cellActiveColor: CellColor = CellColor(Color.red)
-    @Published var cellInactiveColor: CellColor = CellColor(Color.white)
+    @Published var cellActiveColor: CellColor = CellColor.red
+    @Published var cellInactiveColor: CellColor = CellColor.white
 }
