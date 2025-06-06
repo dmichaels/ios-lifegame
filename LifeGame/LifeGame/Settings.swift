244803bf1325468d5067d33065700e1b02909caa
David Michaels <dmichaels@gmail.com>
2025-06-05 22:54:27 -0400

    etc

--- LifeGame/LifeGame/Settings.swift
+++ LifeGame/LifeGame/Settings.swift
@@ -5,56 +5,63 @@ import CellGridView
 
 public class Defaults
 {
-    public static let ignoreSafeArea: Bool              = true
-    public static let automationInterval: Double        = 0.2
+    // CellGridView base class specific properties.
 
-    public static let viewScaling: Bool                 = CellGridView.Defaults.viewScaling
     public static let viewBackground: CellColor         = CellGridView.Defaults.viewBackground
     public static let viewTransparency: UInt8           = CellGridView.Defaults.viewTransparency
+    public static let viewScaling: Bool                 = CellGridView.Defaults.viewScaling
 
     public static let cellSize: Int                     = CellGridView.Defaults.cellSize
     public static let cellSizeFit: Bool                 = CellGridView.Defaults.cellSizeFit
     public static let cellPadding: Int                  = CellGridView.Defaults.cellPadding
     public static let cellShape: CellShape              = CellGridView.Defaults.cellShape
     public static let cellForeground: CellColor         = CellGridView.Defaults.cellForeground
+
+    public static let cellSizeMax: Int                  = CellGridView.Defaults.cellSizeMax
+    public static let cellSizeInnerMin: Int             = CellGridView.Defaults.cellSizeInnerMin
+    public static let cellPaddingMax: Int               = CellGridView.Defaults.cellPaddingMax
+    public static let cellPreferredSizeMarginMax: Int   = CellGridView.Defaults.cellPreferredSizeMarginMax
     public static let cellAntialiasFade: Float          = CellGridView.Defaults.cellAntialiasFade
     public static let cellRoundedRectangleRadius: Float = CellGridView.Defaults.cellRoundedRectangleRadius
-    public static let soundEnabled: Bool                = true
-    public static let hapticEnabled: Bool               = true
+
+    public static let centerCellGrid: Bool              = CellGridView.Defaults.centerCellGrid
+    public static let restrictShiftStrict: Bool         = CellGridView.Defaults.restrictShiftStrict
+    public static let unscaledZoom: Bool                = CellGridView.Defaults.unscaledZoom
+    public static let automationInterval: Double        = CellGridView.Defaults.automationInterval
+
+    // Life Game specific properties.
 
     public static let gridColumns: Int                  = 50
     public static let gridRows: Int                     = 75
 
+    public static let cellActiveColor: CellColor        = CellColor.red
+    public static let cellInactiveColor: CellColor      = CellColor.white
+
     public static let dragThreshold: Int                = 3
     public static let swipeThreshold: Int               = 100
-    public static let updateMode: Bool                  = false
 
-    public static let restrictShiftStrict: Bool         = false
-    public static let centerCellGrid: Bool              = false
-    public static let unscaledZoom: Bool                = false
+    public static let soundEnabled: Bool                = true
+    public static let hapticEnabled: Bool               = true
 
-    public static let cellActiveColor: CellColor = CellColor.red
-    public static let cellInactiveColor: CellColor = CellColor.white
+    public static let ignoreSafeArea: Bool              = true
 }
 
 class Settings: ObservableObject
 {
     @Published var viewBackground: CellColor         = Defaults.viewBackground
+    @Published var viewTransparency: UInt8           = Defaults.viewTransparency
     @Published var viewScaling: Bool                 = Defaults.viewScaling
 
     @Published var cellSize: Int                     = Defaults.cellSize
     @Published var cellSizeFit: Bool                 = Defaults.cellSizeFit
     @Published var cellPadding: Int                  = Defaults.cellPadding
     @Published var cellShape: CellShape              = Defaults.cellShape
-    @Published var cellAntialiasFade: Float          = Defaults.cellAntialiasFade
-    @Published var cellRoundedRectangleRadius: Float = Defaults.cellRoundedRectangleRadius
+
     @Published var soundEnabled: Bool                = Defaults.soundEnabled
     @Published var hapticEnabled: Bool               = Defaults.hapticEnabled
 
     @Published var gridColumns: Int                  = Defaults.gridColumns
     @Published var gridRows: Int                     = Defaults.gridRows
-
-    @Published var updateMode: Bool                  = Defaults.updateMode
 
     @Published var cellActiveColor: CellColor = CellColor.red
     @Published var cellInactiveColor: CellColor = CellColor.white
