042d264d924996f38a53f2a1333bdaf0218a31fb
David Michaels <dmichaels@gmail.com>
2025-06-18 13:26:00 -0400

    etc

--- LifeGame/LifeGame/SettingsView.swift
+++ LifeGame/LifeGame/SettingsView.swift
@@ -10,117 +10,33 @@ struct SettingsView: View
     @EnvironmentObject var cellGridView: LifeCellGridView
     @EnvironmentObject var settings: Settings
 
+    let iconWidth: CGFloat = 32
+
     var body: some View {
         Form {
-            Section(/* header: Text("CELLS").padding(.leading, -12) */ ) {
-                HStack {
-                    Label("Cell Shape", systemImage: "puzzlepiece.fill")
-                    Picker("", selection: $settings.cellShape) {
-                        ForEach(CellShape.allCases) { value in
-                            Text(value.rawValue)
-                                .lineLimit(1)
-                                .truncationMode(.tail)
-                                .tag(value)
+            Section {
+                VStack(alignment: .leading) {
+                    HStack(alignment: .firstTextBaseline) {
+                        Image(systemName: "puzzlepiece.fill").frame(width: iconWidth, alignment: .leading)
+                        Text("Cell Shape").alignmentGuide(.leading) { d in d[.leading] }
+                        Picker("", selection: $settings.cellShape) {
+                            ForEach(CellShape.allCases) { value in
+                                Text(value.rawValue).lineLimit(1).truncationMode(.tail).tag(value)
+                            }
                         }
+                        .pickerStyle(.menu).disabled(settings.cellSize < 6)
                     }
-                    .pickerStyle(.menu)
-                    .disabled(settings.cellSize < 6)
-                }
-                VStack {
-                    HStack {
-                        Label("Cell Size", systemImage: "magnifyingglass")
+                    HStack(alignment: .firstTextBaseline) {
+                        Image(systemName: "magnifyingglass").frame(width: iconWidth, alignment: .leading)
+                        Text("Cell Size").alignmentGuide(.leading) { d in d[.leading] }
                         Spacer()
-                        Text("\(settings.cellSize)")
-                    }
+                        Text("\(settings.cellSize)").foregroundColor(.secondary)
+                    }.padding(.bottom, 8)
                     Slider(
-                        value: Binding(
-                            get: { Double(settings.cellSize) },
-                            set: { settings.cellSize = Int($0) }
-                        ),
-                        in: Double(cellGridView.minimumCellSize)...Double(cellGridView.maximumCellSize), step: 1)
-                        .padding(.top, -8)
-                        .padding(.bottom, -2)
-                        .onChange(of: settings.cellSize) { newValue in
-                            settings.cellSize = newValue
-                        }
-                }
-                HStack {
-                    HStack {
-                        ColorCircleIcon()
-                        Text("Active Color")
-                            .padding(.leading, 0)
-                            .frame(width: 116)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                            .layoutPriority(1)
-                        Spacer()
-                    }
-                    ColorPicker("", selection: $settings.activeColorInternal)
-                        .onChange(of: settings.activeColorInternal) { newValue in
-                           settings.activeColorInternal = newValue
-                        }
-                }
-                HStack {
-                    HStack {
-                        ColorCircleIcon()
-                        Text("Inactive Color")
-                            .padding(.leading, 0)
-                            .frame(width: 128)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                            .layoutPriority(1)
-                        Spacer()
-                    }
-                    ColorPicker("", selection: $settings.inactiveColorInternal)
-                        .onChange(of: settings.inactiveColorInternal) { value in
-                           settings.inactiveColorInternal = value
-                        }
-                }
-                HStack {
-                    Label("Inactive Color Random", systemImage: "questionmark.app.dashed")
-                    Spacer()
-                    Toggle("", isOn: $settings.inactiveColorRandom)
-                        .labelsHidden()
-                }
-                HStack {
-                    Label("Inactive Color Dynamic", systemImage: "circle.grid.cross")
-                    Spacer()
-                    Toggle("", isOn: $settings.inactiveColorRandomDynamic)
-                        .labelsHidden()
-                }
-                HStack {
-                    Image(systemName: "paintpalette")
-                        .foregroundColor(.accentColor)
-                    Text("Inactive Color Mode")
-                        .frame(width: 178)
-                    Picker("", selection: $settings.inactiveColorRandomColorMode) {
-                        ForEach(ColourMode.allCases) { mode in
-                            Text(mode.rawValue)
-                                .lineLimit(1)
-                                // .truncationMode(.tail)
-                                .tag(mode)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .onChange(of: settings.inactiveColorRandomColorMode) { newValue in
-                        settings.inactiveColorRandomColorMode = newValue
-                    }
-                }.disabled(!settings.inactiveColorRandom && !settings.inactiveColorRandomDynamic)
-                HStack {
-                    HStack {
-                        ColorCircleIcon()
-                        Text("Background Color")
-                            .padding(.leading, 8)
-                            .frame(width: 152)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                            .layoutPriority(1)
-                        Spacer()
-                    }
-                    ColorPicker("", selection: $settings.viewBackgroundInternal)
-                        .onChange(of: settings.viewBackgroundInternal) { newValue in
-                           settings.viewBackgroundInternal = newValue
-                        }
+                        value: Binding(get: { Double(settings.cellSize) }, set: { settings.cellSize = Int($0) }),
+                                       in: Double(cellGridView.minimumCellSize)...Double(cellGridView.maximumCellSize), step: 1)
+                        .padding(.top, -8).padding(.bottom, -2)
+                        .onChange(of: settings.cellSize) { newValue in settings.cellSize = newValue }
                 }
                 VStack {
                     HStack {
@@ -159,6 +75,86 @@ struct SettingsView: View
                 //     }
                 //     .pickerStyle(.menu)
                 // }
+            }
+            Section(header: Text("COLORS").padding(.leading, -12).padding(.top, -20)) {
+                HStack {
+                    HStack {
+                        ColorCircleIcon()
+                        Text("Active")
+                            .padding(.leading, 0)
+                            .frame(alignment: .leading)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                            .layoutPriority(1)
+                        Spacer()
+                    }
+                    ColorPicker("", selection: $settings.activeColorInternal)
+                        .onChange(of: settings.activeColorInternal) { newValue in
+                           settings.activeColorInternal = newValue
+                        }
+                }
+                HStack {
+                    HStack {
+                        ColorCircleIcon()
+                        Text("Inactive")
+                            .padding(.leading, 0)
+                            .frame(width: 128)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                            .layoutPriority(1)
+                        Spacer()
+                    }
+                    ColorPicker("", selection: $settings.inactiveColorInternal)
+                        .onChange(of: settings.inactiveColorInternal) { value in
+                           settings.inactiveColorInternal = value
+                        }
+                }
+                HStack {
+                    Label("Inactive Random", systemImage: "questionmark.app.dashed")
+                    Spacer()
+                    Toggle("", isOn: $settings.inactiveColorRandom)
+                        .labelsHidden()
+                }
+                HStack {
+                    Label("Inactive Dynamic", systemImage: "circle.grid.cross")
+                    Spacer()
+                    Toggle("", isOn: $settings.inactiveColorRandomDynamic)
+                        .labelsHidden()
+                }.disabled(!settings.inactiveColorRandom)
+                HStack {
+                    Image(systemName: "paintpalette")
+                        .foregroundColor(.accentColor)
+                    Text("Inactive Color Mode")
+                        .frame(width: 178)
+                    Picker("", selection: $settings.inactiveColorRandomColorMode) {
+                        ForEach(ColourMode.allCases) { mode in
+                            Text(mode.rawValue)
+                                .lineLimit(1)
+                                // .truncationMode(.tail)
+                                .tag(mode)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .onChange(of: settings.inactiveColorRandomColorMode) { newValue in
+                        settings.inactiveColorRandomColorMode = newValue
+                    }
+                }.disabled(!settings.inactiveColorRandom)
+                HStack {
+                    HStack {
+                        ColorCircleIcon()
+                        Text("Background")
+                            .padding(.leading, 8)
+                            .frame(width: 152)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                            .layoutPriority(1)
+                        Spacer()
+                    }
+                    ColorPicker("", selection: $settings.viewBackgroundInternal)
+                        .onChange(of: settings.viewBackgroundInternal) { newValue in
+                           settings.viewBackgroundInternal = newValue
+                        }
+                }
             }
         }
         .offset(y: -30)
