3c1d66ddcb72a8d165d9b0a27c8bb5fa19d887ba
David Michaels <dmichaels@gmail.com>
2025-06-18 19:17:05 -0400

    etc

--- LifeGame/LifeGame/SettingsView.swift
+++ LifeGame/LifeGame/SettingsView.swift
@@ -159,29 +159,3 @@ let AutomationIntervalOptions: [(label: String, value: Double)] = [
     ("Wow", 0.05),
     ("Max", 0.0)
 ]
-
-internal struct IconLabel: View {
-    private var _text: String
-    private var _icon: String
-    private var _iconWidth: CGFloat
-    internal init(_ text: String, _ icon: String, iconWidth: CGFloat = 32.0) {
-        self._text = text
-        self._icon = icon
-        self._iconWidth = iconWidth
-    }
-    internal var body: some View {
-        HStack(alignment: .center, spacing: 8) {
-            if (self._icon == "COLOR") {
-                ColorCircleIcon().frame(width: self._iconWidth, alignment: .leading)
-            }
-            else {
-                Image(systemName: self._icon).frame(width: self._iconWidth, alignment: .leading)
-            }
-            Text(self._text)
-                .alignmentGuide(.leading) { d in d[.leading] }
-                .fixedSize()
-                .layoutPriority(1)
-            Spacer()
-        }
-    }
-}
