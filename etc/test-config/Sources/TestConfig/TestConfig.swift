ca33e61200f06e3e714eccdc5b2ccb9196b949f2
David Michaels <dmichaels@gmail.com>
2025-06-24 11:34:51 -0400

    etc

--- etc/test-config/Sources/TestConfig/TestConfig.swift
+++ etc/test-config/Sources/TestConfig/TestConfig.swift
@@ -1,26 +1,17 @@
-// FILE: ios-cellgridview/CellGridView.swift
+// ---------------------------------------------------------------------------------------------------------------------
+// ios-cellgridview
+// ---------------------------------------------------------------------------------------------------------------------
+
+// FILE: ios-cellgridview/CellGridView+Defaults.swift
 //
-public class CellGridView
+extension CellGridView
 {
-    private var _cellSize: Int
-    private var _cellPadding: Int
-
-    public var   cellSize: Int    { self._cellSize }
-    public var   cellPadding: Int { self._cellPadding }
-
-    public init(_ config: CellGridView.Config? = nil) {
-        self._cellSize = config?.cellSize ?? CellGridView.Defaults.cellSize
-        self._cellPadding = config?.cellPadding ?? CellGridView.Defaults.cellPadding
-    }
-
-    open var config: CellGridView.Config {
-        CellGridView.Config(self)
-    }
-
-    open func configure(_ config: CellGridView.Config) {
-        self._cellSize = config.cellSize
-        self._cellPadding = config.cellPadding
-        print("CellGridView.configure")
+    public class Defaults {
+        public static let viewBackground: Int     = 123
+        public static let viewTransparency: UInt8 = 255
+        public static let viewScaling: Bool       = true
+        public static let cellSize: Int           = 43
+        public static let cellPadding: Int        = 2
     }
 }
 
@@ -28,22 +19,94 @@ public class CellGridView
 //
 extension CellGridView
 {
-    public class Defaults {
-        public static let cellSize: Int    = 43
-        public static let cellPadding: Int = 2
-    }
-
     public class Config {
 
+        public var viewBackground: Int
+        public var viewTransparency: UInt8
+        public var viewScaling: Bool
         public var cellSize: Int
         public var cellPadding: Int
 
         public init(_ cellGridView: CellGridView? = nil) {
-            self.cellSize    = cellGridView?.cellSize    ?? CellGridView.Defaults.cellSize
-            self.cellPadding = cellGridView?.cellPadding ?? CellGridView.Defaults.cellPadding
+            self.viewBackground   = cellGridView?.viewBackground   ?? CellGridView.Defaults.viewBackground
+            self.viewTransparency = cellGridView?.viewTransparency ?? CellGridView.Defaults.viewTransparency
+            self.viewScaling      = cellGridView?.viewScaling      ?? CellGridView.Defaults.viewScaling
+            self.cellSize         = cellGridView?.cellSize         ?? CellGridView.Defaults.cellSize
+            self.cellPadding      = cellGridView?.cellPadding      ?? CellGridView.Defaults.cellPadding
         }
     }
+}
 
+// FILE: ios-cellgridview/CellGridView.swift
+//
+public class CellGridView
+{
+    private var _viewBackground: Int
+    private var _viewTransparency: UInt8
+    private var _viewScaling: Bool
+    private var _cellSize: Int
+    private var _cellPadding: Int
+
+    public var   viewBackground: Int     { self._viewBackground }
+    public var   viewTransparency: UInt8 { self._viewTransparency }
+    public var   viewScaling: Bool       { self._viewScaling }
+    public var   cellSize: Int           { self._cellSize }
+    public var   cellPadding: Int        { self._cellPadding }
+
+    public init(_ config: CellGridView.Config? = nil) {
+        self._viewBackground   = config?.viewBackground   ?? CellGridView.Defaults.viewBackground
+        self._viewTransparency = config?.viewTransparency ?? CellGridView.Defaults.viewTransparency
+        self._viewScaling      = config?.viewScaling      ?? CellGridView.Defaults.viewScaling
+        self._cellSize         = config?.cellSize         ?? CellGridView.Defaults.cellSize
+        self._cellPadding      = config?.cellPadding      ?? CellGridView.Defaults.cellPadding
+    }
+
+    open var config: CellGridView.Config {
+        CellGridView.Config(self)
+    }
+
+    open func initialize(_ config: CellGridView.Config, fit: Bool = false, center: Bool = false) {
+        self.configure(config)
+    }
+
+    open func configure(_ config: CellGridView.Config) {
+        self._viewBackground   = config.viewBackground
+        self._viewTransparency = config.viewTransparency
+        self._viewScaling      = config.viewScaling
+        self._cellSize         = config.cellSize
+        self._cellPadding      = config.cellPadding
+        print("CellGridView.configure")
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ios-lifegame
+// ---------------------------------------------------------------------------------------------------------------------
+
+// FILE: ios-lifegame/LifeCellGridView+Defaults.swift
+//
+extension LifeCellGridView
+{
+    class Defaults {
+        public static let viewBackground: Int = CellGridView.Defaults.viewBackground
+        public static let viewScaling: Bool   = CellGridView.Defaults.viewScaling
+        public static let cellSize: Int       = 25
+        public static let cellPadding: Int    = 1
+        public static let activeColor: Int    = 0x06
+        public static let inactiveColor: Int  = 0x07
+    }
+}
+
+// FILE: ios-lifegame/Settings.swift
+//
+class Settings
+{
+    public var viewBackground: Int = CellGridView.Defaults.viewBackground
+    public var viewScaling: Bool   = CellGridView.Defaults.viewScaling
+    public var cellSize: Int       = CellGridView.Defaults.cellSize
+    public var cellPadding: Int    = CellGridView.Defaults.cellPadding
+    public var activeColor: Int    = LifeCellGridView.Defaults.activeColor
+    public var inactiveColor: Int  = LifeCellGridView.Defaults.inactiveColor
 }
 
 // FILE: ios-lifegame/LifeCellGridView.swift
@@ -53,7 +116,7 @@ public class LifeCellGridView: CellGridView {
     private var _activeColor: Int
     private var _inactiveColor: Int
 
-    internal var activeColor: Int { self._activeColor }
+    internal var activeColor: Int   { self._activeColor }
     internal var inactiveColor: Int { self._inactiveColor }
 
     public override init(_ config: CellGridView.Config? = nil) {
@@ -65,6 +128,10 @@ public class LifeCellGridView: CellGridView {
 
     public override var config: LifeCellGridView.Config {
         LifeCellGridView.Config(self)
+    }
+
+    public override func initialize(_ config: CellGridView.Config, fit: Bool = false, center: Bool = false) {
+        self.configure(config)
     }
 
     public override func configure(_ config: CellGridView.Config) {
@@ -79,13 +146,6 @@ public class LifeCellGridView: CellGridView {
 //
 extension LifeCellGridView
 {
-    class Defaults {
-        public static let cellSize: Int      = 25
-        public static let cellPadding: Int   = 1
-        public static let activeColor: Int   = 0x06
-        public static let inactiveColor: Int = 0x07
-    }
-
     public class Config: CellGridView.Config {
 
         public var activeColor: Int
@@ -95,8 +155,10 @@ extension LifeCellGridView
             self.activeColor   = cellGridView?.activeColor   ?? LifeCellGridView.Defaults.activeColor
             self.inactiveColor = cellGridView?.inactiveColor ?? LifeCellGridView.Defaults.inactiveColor
             super.init(cellGridView)
-            self.cellSize      = cellGridView?.cellSize      ?? LifeCellGridView.Defaults.cellSize
-            self.cellPadding   = cellGridView?.cellPadding   ?? LifeCellGridView.Defaults.cellPadding
+            self.viewBackground = cellGridView?.viewBackground ?? LifeCellGridView.Defaults.viewBackground
+            self.viewScaling    = cellGridView?.viewScaling    ?? LifeCellGridView.Defaults.viewScaling
+            self.cellSize       = cellGridView?.cellSize       ?? LifeCellGridView.Defaults.cellSize
+            self.cellPadding    = cellGridView?.cellPadding    ?? LifeCellGridView.Defaults.cellPadding
         }
     }
 }
@@ -114,10 +176,12 @@ extension Settings
     //
     public func fromConfig(_ config: LifeCellGridView.Config)
     {
-        self.cellSize = config.cellSize
-        self.cellPadding = config.cellPadding
-        self.activeColor = config.activeColor
-        self.inactiveColor = config.inactiveColor
+        self.viewBackground = config.viewBackground
+        self.viewScaling    = config.viewScaling
+        self.cellSize       = config.cellSize
+        self.cellPadding    = config.cellPadding
+        self.activeColor    = config.activeColor
+        self.inactiveColor  = config.inactiveColor
     }
 
     // Creates and returns a LifeCellGridView.Config (derived from CellGridView.Config) object,
@@ -132,22 +196,14 @@ extension Settings
     internal func toConfig(_ cellGridView: LifeCellGridView) -> LifeCellGridView.Config
     {
         let config: LifeCellGridView.Config = LifeCellGridView.Config(cellGridView)
-        config.cellSize      = self.cellSize
-        config.cellPadding   = self.cellPadding
-        config.activeColor   = self.activeColor
-        config.inactiveColor = self.inactiveColor
+        config.viewBackground = self.viewBackground
+        config.viewScaling    = self.viewScaling
+        config.cellSize       = self.cellSize
+        config.cellPadding    = self.cellPadding
+        config.activeColor    = self.activeColor
+        config.inactiveColor  = self.inactiveColor
         return config
     }
-}
-
-// FILE: ios-lifegame/Settings.swift
-//
-class Settings
-{
-    public var cellSize: Int      = CellGridView.Defaults.cellSize
-    public var cellPadding: Int   = CellGridView.Defaults.cellPadding
-    public var activeColor: Int   = LifeCellGridView.Defaults.activeColor
-    public var inactiveColor: Int = LifeCellGridView.Defaults.inactiveColor
 }
 
 // FILE: ios-lifegame/ContentView.swift
